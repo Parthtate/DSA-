099ba8ef1464d5ce2de21add5a165d7ab8a1d80a
Parth Tate <113414261+Parthtate@users.noreply.github.com>
2024-06-26 10:39:58 +0530

    Update DoublyLinkedList.js

--- linkedlist/DoublyLinkedList.js
+++ linkedlist/DoublyLinkedList.js
@@ -22,7 +22,7 @@ DoublyLinkedList.prototype.insertAtBeginning = function(data){
     // check if already have a data present in DLL
     if (this.head !== null) {
         this.head.prev = newNode // if already data present in DLL, attached newNode at prev which is null, then update this.head.
-        this.head = newNode // update head
+        // this.head = newNode // update head
     }
     this.head = newNode
 
@@ -32,5 +32,43 @@ DoublyLinkedList.prototype.insertAtBeginning = function(data){
 
         // optional (create circular list) update
         // this.tail = this.head
+    }
+}
+
+// insert at the end
+DoublyLinkedList.prototype.insertAtEnd = function(data){
+    const newNode = new Node(data, null, this.tail)
+
+    //if tail is not empty.
+    if(this.tail !== null){
+        this.tail.next = newNode
+    }
+    // else if this.tail is empty
+    this.tail = newNode
+
+    if(this.head === null){
+        this.head = newNode
+    }
+}
+
+// insert at given node.
+DoublyLinkedList.prototype.insertAfterNode = function(prevNode, data){
+    if(prevNode == null){
+        console.log("Invalid prevNode!");
+        return
+    }
+
+    const newNode = new Node(data, prev.next, prev)
+
+    // check if prevNode is not empty.
+    if(prevNode.next !== null){
+        prevNode.next.prev = newNode
+    }
+    // else prevNode == null
+    prevNode.next = newNode
+
+    // insert node at tail
+    if(newNode.next == null){
+        this.tail = newNode
     }
 }
