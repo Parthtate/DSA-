851cae2a4551699ffa02967b3bd0f8e39acfd4af
Parth Tate <113414261+Parthtate@users.noreply.github.com>
2024-06-29 06:14:43 +0000

    #

---
+++ Tree/01_tree.js
@@ -0,0 +1,20 @@
+/* Tree:
+1. Tree data structure is same like doubly linkedlist, it hold 2 values left & right.
+2. Topmost node called root node, it hold any value. form root node created 0 or less than 2 subchild left hold lesser value and right hold bigger value compare to root node value.
+3. Recurssion will be use by tree, it divide node into subtree for compare and easy to traverse the tree.
+4. 
+*/
+
+class Node {
+    constructor(node){
+        this.node = node
+        this.left = left
+        this.right = right
+    }
+}
+
+class Tree {
+    constructor(){
+        
+    }
+}
