0100ed3546e1cd75d2a8a1f78ac572e86fdde603
Parth Tate <113414261+Parthtate@users.noreply.github.com>
2024-07-26 05:19:39 +0000

    #

---
+++ 06_DynamicProgramming/01_DPBasic.js
@@ -0,0 +1,18 @@
+/* Dynamic Programming
+DP is used for inhanced our program or better for optimization. Before knowing Dp, we know some graph, recursion concept.
+to become more easy to understand Dynamic Programming.
+
+There are 2 main types of DP
+1. Top-Down approach (Memoization)
+2. Bottom-Up approach (Tabulation)
+we store result in table (array, matrix, variable) or object. depends on which progamming language.
+
+1. Memoization
+-> Break problems at the top.
+-> Recursive -> with table to store result.
+
+2. Tabulation
+-> consider smallest problem first.
+-> solution are typically build iteratively and are stored in a table (array)
+
+*/
